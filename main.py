895407cdd8ea692bed08bede9b9e27b042587db5
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-03-13 14:08:21 -0500

    final changes

---
+++ main.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pandas as pd
+import sklearn
+from sklearn import svm
+from sklearn import metrics
+from sklearn.neighbors import KNeighborsClassifier
+df = pd.read_csv("iris.data")
+
+print("Before replacing:\n", df.head())
+df["class"] = df["class"].replace("Iris-setosa", 0)
+df["class"] = df["class"].replace("Iris-versicolor", 1)
+df["class"] = df["class"].replace("Iris-virginica", 2)
+print("After replacing:\n", df.head())
+
+predict = "class"
+
+x = np.array(df.drop(columns = [predict]))
+y = np.array(df[predict])
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size= 0.2)
+
+
+
+
+clf = svm.SVC()
+clf.fit(x_train, y_train)
+y_pred = clf.predict(x_test)
+acc = metrics.accuracy_score(y_test, y_pred)
+print("SVM(without kernel):", format(acc, "%"))
+
+
+# use one kernel: linear with soft/hard margin or poly with degree
+clf = svm.SVC(kernel = "linear", C=1, degree = 3)
+clf.fit(x_train, y_train)
+y_pred = clf.predict(x_test)
+accuracy = metrics.accuracy_score(y_test, y_pred)
+# 98.245614%
+print("SVM(with kernel):", format(accuracy, "%"))
