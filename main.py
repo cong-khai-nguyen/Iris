f15751817b21439b55d4e4581a287f9ec1420153
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-03-13 14:10:45 -0500

    final changes

--- main.py
+++ main.py
@@ -17,22 +17,23 @@ predict = "class"
 x = np.array(df.drop(columns = [predict]))
 y = np.array(df[predict])
 
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size= 0.2)
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size= 0.1)
 
 
 
-
+# Use SVC without any parameters
 clf = svm.SVC()
 clf.fit(x_train, y_train)
 y_pred = clf.predict(x_test)
 acc = metrics.accuracy_score(y_test, y_pred)
+#96.666667%
 print("SVM(without kernel):", format(acc, "%"))
 
 
 # use one kernel: linear with soft/hard margin or poly with degree
-clf = svm.SVC(kernel = "linear", C=1, degree = 3)
+clf = svm.SVC(kernel = "linear", C=1)
 clf.fit(x_train, y_train)
 y_pred = clf.predict(x_test)
 accuracy = metrics.accuracy_score(y_test, y_pred)
-# 98.245614%
+# 100.000000%
 print("SVM(with kernel):", format(accuracy, "%"))
